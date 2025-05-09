a92bc2d16ecff8184ad2e08a6b7b7f5465879d26
MisaHariRabe <misaharirabe58@gmail.com>
2025-05-09 11:58:44 +0300

    update title and implement student form functionality in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    NgIf,
+    NgFor
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'notes-angular';
+  student = {
+    picture: '',
+    nom: '',
+    prenom: '',
+    dateDeNaissance: '',
+    classe: 'CE1'
+  };
+
+  students: { picture: string; nom: string; prenom: string; dateDeNaissance: string; classe: string; }[] = [];
+
+  isFormValid() {
+    return this.student.picture && this.student.nom && this.student.prenom && this.student.dateDeNaissance && this.student.classe
+  }
+
+  title = 'school-manager';
+
+  onSubmit() {
+    if (this.isFormValid()) {
+      this.students.push(this.student);
+      this.student = {
+        picture: '',
+        nom: '',
+        prenom: '',
+        dateDeNaissance: '',
+        classe: 'CE1'
+      };
+    } else {
+      alert("Tous les champs sont requis.");
+    }
+  }
 }
